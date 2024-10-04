0e1c276aae985ae0b591d8413b5d0f17b923de68
codecrafters-2023 <145508415+codecrafters-2023@users.noreply.github.com>
2024-10-05 02:58:15 +0530

    Update Register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -36,7 +36,7 @@ const Register = () => {
       }
     }
 
-    const res = await axios.post("/register", formData, config);
+    const res = await axios.post("https://image-0qwp.onrender.com/register", formData, config);
 
     if (res.data.status === 401 || !res.data) {
       console.log("errror")
